15365df30bd954a03d416415834d154bcb4a8aeb
Chris Wright <cawright@scu.edu>
2018-06-27 11:49:53 -0700

    Achieved parity with demo from lecture

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Chris Wright on 6/27/18.
+//  Copyright © 2018 Chris Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
